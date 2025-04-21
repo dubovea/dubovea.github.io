c06023dad538b3a8905b3b9ef1cba7e5e9d65bf8
EDubov <Edubov@project.ibs.ru>
2025-04-21 12:43:49 +0500

    update bot

--- telegram/bot.js
+++ telegram/bot.js
@@ -1,27 +1,36 @@
-const { Telegraf } = require('telegraf');
+const { Telegraf } = require("telegraf");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const bot = new Telegraf('YOUR_BOT_TOKEN');
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 // Команда для старта
-bot.command('start', (ctx) => {
-    ctx.reply('Добро пожаловать в викторину! Нажмите кнопку ниже, чтобы начать.', {
-        reply_markup: {
-            inline_keyboard: [
-                [{
-                    text: 'Начать викторину',
-                    web_app: { url: 'https://username.github.io' }
-                }]
-            ]
-        }
-    });
+bot.command("start", (ctx) => {
+  ctx.reply(
+    "Добро пожаловать в викторину! Нажмите кнопку ниже, чтобы начать.",
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Начать викторину",
+              web_app: { url: process.env.WEBAPP_URL },
+            },
+          ],
+        ],
+      },
+    }
+  );
 });
 
 // Обработка данных из WebApp
-bot.on('message', (ctx) => {
-    if (ctx.message.web_app_data) {
-        const data = JSON.parse(ctx.message.web_app_data.data);
-        ctx.reply(`Вы завершили викторину с результатом: ${data.score} из ${data.total}!`);
-    }
+bot.on("message", (ctx) => {
+  if (ctx.message.web_app_data) {
+    const data = JSON.parse(ctx.message.web_app_data.data);
+    ctx.reply(
+      `Вы завершили викторину с результатом: ${data.score} из ${data.total}!`
+    );
+  }
 });
 
 bot.launch();
